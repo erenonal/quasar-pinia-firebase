b69bd95efdec86b0dc82d60a6e6cba3780fd2b08
Eren Önal <75484033+erenonal@users.noreply.github.com>
2024-05-13 17:28:10 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import { useUsersStore } from './stores/UsersStore';
+const UsersStore = useUsersStore()
+const updateEmail = (newEmail) => {
+  UsersStore.updateMailAddress(newEmail);
+}
+</script>
+
+<template>
+   <div>
+    <q-btn label="eren" @click="updateEmail('new@example.com')"></q-btn>
+    <h1 style="color: red">{{ UsersStore.mailAddress }}</h1>
+  </div>
+</template>
+
+<style scoped>
+</style>
